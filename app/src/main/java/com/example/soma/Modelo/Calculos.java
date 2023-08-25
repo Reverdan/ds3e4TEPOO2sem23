f1010c1cfd159e87db7bcab123d9b93958c3552f
Reverdan Almeida Sparinger <reverdan@gmail.com>
2023-08-24 21:34:33 -0300

    Manutencao
    Correção bug x

--- app/src/main/java/com/example/soma/Modelo/Calculos.java
+++ app/src/main/java/com/example/soma/Modelo/Calculos.java
@@ -5,6 +5,8 @@ public class Calculos
     public String resultado;
     public void somar(Double n1, Double n2)
     {
-        this.resultado = String.valueOf(n1 + n2);
+        Double resposta = 0.0;
+        resposta = n1 + n2;
+        this.resultado = String.valueOf(resposta);
     }
 }
